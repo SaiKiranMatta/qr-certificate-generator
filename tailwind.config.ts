eda46f61d9ed2b3b12676e8d0799f3e869fb1754
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-11-04 02:18:39 +0530

    chore: update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,6 +55,19 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		animation: {
+  			grid: 'grid 15s linear infinite'
+  		},
+  		keyframes: {
+  			grid: {
+  				'0%': {
+  					transform: 'translateY(-50%)'
+  				},
+  				'100%': {
+  					transform: 'translateY(0)'
+  				}
+  			}
   		}
   	}
   },
